0d0fbbf4bcc2835b89de2ac474526ecdc63724cb
Sijey <cjpraveen@hotmail.com>
2022-08-15 18:10:42 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -91,6 +91,8 @@ const Box = styled.div`
         border-radius: 1em;
         height: 256px;
         width: 256px;
+        object-fit: cover;
+        object-position: center;
         box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
         transition: all 0.3s cubic-bezier(.25,.8,.25,1);
 
