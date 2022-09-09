e13c1f66cca2a2c2d4a8f492554510a1c274c10c
Mahi <mahichowdhury587@gmail.com>
2022-09-09 18:00:05 +0600

    Added Contributors and Genre section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './Components/HomePage/HomePage';
 import NavBar from "./Components/NavBar/NavBar";
+import Genre from "./Components/HomePage/Genre/Genre";
+import Contributors from "./Components/HomePage/Contributors/Contributors";
 
 
 function App() {
@@ -12,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
-          
+        <Genre />
+        <Contributors />
       </Router>
     </div>
   );
