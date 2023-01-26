36fc03748ad7b438532aba18063485262aa2c584
Mahi <mahichowdhury587@gmail.com>
2023-01-26 11:51:58 +0600

    improvement in result

--- src/Components/HomePage/Hero/Hero.jsx
+++ src/Components/HomePage/Hero/Hero.jsx
@@ -24,6 +24,7 @@ const Hero = () => {
           }
       });
       const data = await response.json();
+      console.log(data)
       setImages(data);
     } catch (error) {
       console.log(error);
--- src/Components/HomePage/SearchResults/SearchResult.jsx
+++ src/Components/HomePage/SearchResults/SearchResult.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
 const SearchResult = ({  image }) => {
+  console.log(image)
+
+  if (!Array.isArray(image) || !image.length) {
+    return null;
+  }
+
   return (
     <div>
       {image.map((r) => (
         <div key={r.id}>
           <img width="30%" src={r.src} alt={r.alt} />
         </div>
+        
       ))}
+       
     </div>
+  
   )
 }
+
+
 
 export default SearchResult
